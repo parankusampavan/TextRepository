860ab10e7adb408eccb7877b7ab29713de198f8c
Pavan <Pavan@DESKTOP-140SBE7>
2018-05-31 02:27:29 +0530

    This is commit for master changes in master branch

--- src/main/java/Utils/App.java
+++ src/main/java/Utils/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World test master Branch!" );
+        System.out.println( "Hello World test changes in master branch" );
     }
 }
